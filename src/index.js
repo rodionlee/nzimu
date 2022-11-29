50a6d2ae84e2f70750b2aad346514133ec2fe0b5
rodionion <rodionvh@gmail.com>
2022-11-29 22:06:25 +0200

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// import { AsyncDependenciesBlock } from 'webpack';
+import "./style.css";
+
+console.log("123");
+console.log("123");
+
+// const asdasd = 2;
+
+const test = function () {
+  console.log("123");
+};
+
+function myFunc() {
+  () => {
+    console.log("test");
+    if (true) console.log("312");
+    else console.log("222");
+  };
+}
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  devServer: {
+    static: "./dist",
+    host: "0.0.0.0",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "[name][ext]",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Custom template",
+      template: "./src/index.html",
+    }),
+  ],
+  optimization: {
+    runtimeChunk: "single",
+  },
+};
