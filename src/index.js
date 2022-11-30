fdca6490294ae69aae0e8ee7791a1e258791ea1c
rodionion <rodionvh@gmail.com>
2022-11-30 20:58:58 +0200

    Initial setup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
 // import { AsyncDependenciesBlock } from 'webpack';
 import "./style.css";
+// import "../graphics/123.jpg"
 
-console.log("123");
-console.log("123");
-
-// const asdasd = 2;
-
-const test = function () {
-  console.log("123");
-};
-
-function myFunc() {
-  () => {
-    console.log("test");
-    if (true) console.log("312");
-    else console.log("222");
-  };
-}
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,20 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+        use: "file-loader"
       },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
     ],
   },
   plugins: [
