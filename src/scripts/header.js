f5d377257a3e98884e0d8919d98fab2777f2aa98
rodionion <rodionvh@gmail.com>
2022-12-22 18:33:02 +0200

    Add About page

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -4,6 +4,7 @@ import "../styles/about.css";
 import "../styles/services.css";
 import "../styles/service.css";
 import "../styles/contact.css";
+import "../styles/blog.css";
 
 import "../graphics/image-icon.svg";
 
